8ff36a5cbd517eeb9d359535e6ddbd91ea769118
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2023-08-18 14:21:12 -0700

    created main page

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,17 @@
+function RightSidebar () {
+    return (
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">
+                    Sugested Comunities</h3>
+            </div>
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">
+                    Sugested Users</h3>
+            </div>
+        </section>
+        
+    )
+}
+
+export default RightSidebar;
