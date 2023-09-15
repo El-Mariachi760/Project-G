8905b4a3c6919becd99549ba9d9d7d69d2b034a8
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2023-09-15 14:03:52 -0700

    Added activity-page and functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model";
 import { FilterQuery, SortOrder } from "mongoose";
+import { Children } from "react";
 
 interface Params {
     userId: string;
@@ -138,5 +139,33 @@ export async function fetchUsers({
 
     } catch (error: any) {
         throw new Error(`Failed to fetch users: ${error.message}`)
+    }
+}
+
+export async function getActivity(userId: string) {
+    try {
+        connectToDB();
+
+        // Find all threads created by the user
+        const userThreads = await Thread.find({ author: userId });
+
+        // Collect all child thread ids
+        const childThreadIds = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children);
+        }, [])
+
+        const replies = await Thread.find({
+            _id: { $in: childThreadIds },
+            author: { $ne: userId }
+        }).populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        })
+
+        return replies;
+
+    } catch (error: any) {
+        throw new Error(`Failed to fetch Activity: ${error.message}`)
     }
 }
